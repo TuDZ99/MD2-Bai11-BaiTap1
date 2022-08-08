b9c69bb548c43dcf7b6df114893458c8785da41f
TuDZ99 <tubuithanh99@icloud.com>
2022-08-08 21:51:28 +0700

    baitap1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "đi qua mùa cô đơn !!!";
+        str = str.replaceAll(",", "");
+        String[] arr = str.split(" ");
+        System.out.println(str);
+        System.out.println("------------");
+        Map<String, Integer> map = new TreeMap<String, Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].toUpperCase();
+            String key = arr[i];
+            if (map.containsKey(key)) {
+                int value = map.get(key);
+                map.put(key, value + 1);
+            } else {
+                map.put(key, 1);
+            }
+            System.out.println(" từ khóa " + key + " xuất hiện " + map.get(key));
+        }
+        Set tapHop = map.keySet();
+    }
+}
